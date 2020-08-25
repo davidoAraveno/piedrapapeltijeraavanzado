a0a0e47df9cd712afd2fb73a26056473bf35e23a
david aravena <david.aravenap@gmail.com>
2020-08-25 00:37:38 -0400

    Programa piedra papel tijera avanzado terminado

--- piedraPapelTijeraAvanzado.rb
+++ piedraPapelTijeraAvanzado.rb
@@ -1,23 +1,25 @@
+# ///////////////////////////////////////TURNO JUGADOR 1////////////////////////////////
+
 puts "Tu turno jugador 1"
 puts "Eije el numero de la opcion que quieres lanzar"
 puts "\t1. Piedra"
 puts "\t2. Papel"
-puts "\t3. Piedra"
+puts "\t3. Tijera"
 puts "\t4. Terminar el juego"
 jugador_1 = gets.chomp.to_i
-
 
 def validacion_jugador1(numero_jugador1)
     if numero_jugador1 == 1 || numero_jugador1 == 2 || numero_jugador1 == 3
         return numero_jugador1
     elsif numero_jugador1 == 4
+        puts "Juego terminado por el jugador 1"
         exit
     else
-        puts "Elegiste una opcion no valida. Vuelve a intentarlo jugador 1"
+        puts "Jugador 1 elegiste una opcion no valida. Vuelve a intentarlo jugador 1"
         puts "Eije el numero de la opcion que quieres lanzar"
         puts "\t1. Piedra"
         puts "\t2. Papel"
-        puts "\t3. Piedra"
+        puts "\t3. Tijera"
         jugador_1 = gets.chomp.to_i
         validacion_jugador1(jugador_1)
     end
@@ -26,13 +28,12 @@ end
 jugador1_validado = validacion_jugador1(jugador_1)
 
 
-# /////////////////////////////////////////////////////////////////////////
-
+# //////////////////////////////////TURNO JUGADOR 2///////////////////////////////////////
 puts "Tu turno jugador 2"
-puts "Eije el numero de la opcion que quieres lanzar"
+puts "Elije el numero de la opcion que quieres lanzar"
 puts "\t1. Piedra"
 puts "\t2. Papel"
-puts "\t3. Piedra"
+puts "\t3. Tijera"
 puts "\t4. Terminar el juego"
 jugador_2 = gets.chomp.to_i
 
@@ -40,9 +41,10 @@ def validacion_jugador2(numero_jugador2)
     if numero_jugador2 == 1 || numero_jugador2 == 2 || numero_jugador2 == 3
         return numero_jugador2
     elsif numero_jugador2 == 4
+        puts "Juego terminado por el Jugador 2"
         exit
     else
-        puts "Elegiste una opcion no valida. Vuelve a intentarlo jugador 2"
+        puts "Jugador 2 elegiste una opcion no valida. Vuelve a intentarlo jugador 2"
         puts "Eije el numero de la opcion que quieres lanzar"
         puts "\t1. Piedra"
         puts "\t2. Papel"
@@ -54,8 +56,26 @@ end
 
 jugador2_validado = validacion_jugador2(jugador_2)
 
-puts "jugador1 eligio: #{jugador1_validado}"
-puts "jugador2 eligio: #{jugador2_validado}"
+
+# ////////////////////////////////////////CRITERIO ELECCION GANADOR//////////////////////////////
+case jugador1_validado
+when 1
+    puts "El ganador es el jugador 1 \n Piedra vence a Tijera" if jugador2_validado == 3
+    puts "El ganador es el jugador 2 \n Papel vence a Piedra" if jugador2_validado == 2
+    puts "Empate\n Los dos jugadores eligieron lo mismo" if jugador2_validado == 1
+when 2
+    puts "El ganador es el jugador 1 \n Papel vence a Piedra" if jugador2_validado == 1
+    puts "El ganador es el jugador 2 \n Tijera vence a Papel" if jugador2_validado == 3
+    puts "Empate \n Los dos jugadores eligieron lo mismo" if jugador2_validado == 2
+when 3
+    puts "El ganador es el jugador 1 \n Tijera vence a Papel" if jugador2_validado == 2
+    puts "EL ganador es el jugador 2 \n Piedra vence a Tijera" if jugador2_validado == 1
+    puts "Empate \n Los dos jugadores eligieron lo mismo" if jugador2_validado == 3
+end
+
+
+# ////////////////////////////////////////////MENSAJE CIERRE DE JUEGO///////////////////////////////////////////
+puts "Juego terminado"
 
 
 
